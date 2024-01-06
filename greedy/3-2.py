c10441dfb7b4487169960efa422a86f13c32fc4d
Replit user <>
2024-01-06 18:15:46 +0000

    test

--- greedy/3-2.py
+++ greedy/3-2.py
@@ -1,11 +1,34 @@
 # 큰수의 법칙
-n,m,k = map(int, input().split())
+'''n,m,k = map(int, input().split())
 data = list(map(int, input().split()))
 
-data.sort(reverse=False)
-
+data.sort(reverse=True)
+print(data)
 sum = 0
-l = len(data)
-for data in range(l):
+
+while True:
+  for i in range(k):
+    sum += data[0]
+    m -= 1
+    if m == 0:
+      break
+  sum += data[1]
+  m -= 1
+  if m == 0:
+    break
+
+print(sum)'''
+
+# m의 크기가 100억 이상으로 커지면 시간초과 판정을 받을 것임. 따라서 아래와 같이 수식으로 해결함
+
+n,m,k = map(int, input().split())
+data = list(map(int, input().split()))
   
+data.sort(reverse=True)
+
+result = data[0] * k + data[1]
+result *= int(m / k+1)
+result += data[0] * (m % (k + 1))
+
+print(result)
 
