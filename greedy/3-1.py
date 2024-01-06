b250b2ff20fc2983232ea21dda84214e2dbac84b
Replit user <>
2024-01-06 16:53:24 +0000

    first commit

--- greedy/3-1.py
+++ greedy/3-1.py
@@ -1,7 +1,11 @@
+# 거스름돈
 n=1260
 count=0
+
+#큰 단위의 화폐부터
 array=[500,100,50,10]
+
 for coin in array:
-  count+=n//coin
+  count+=n//coin 
   n%=coin
 print(count)
---
+++ greedy/3-2.py
@@ -0,0 +1,11 @@
+# 큰수의 법칙
+n,m,k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort(reverse=False)
+
+sum = 0
+l = len(data)
+for data in range(l):
+  
+
